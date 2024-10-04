4101795311b318c07cdae9078b92127c3aaf03e4
khannaanurag1 <khannaanurag1@gmail.com>
2024-10-04 14:30:22 +0530

    Created blank project

---
+++ src/main/java/org/example/userauthenticationservice_sept2024/UserAuthenticationServiceSept2024Application.java
@@ -0,0 +1,13 @@
+package org.example.userauthenticationservice_sept2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationServiceSept2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthenticationServiceSept2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/userauthenticationservice_sept2024/UserAuthenticationServiceSept2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.userauthenticationservice_sept2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationServiceSept2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
